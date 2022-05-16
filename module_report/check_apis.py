5761d98d344114ca2b2346a6cb7233307aec1936
Daelon Suzuka <daelonsuzuka@gmail.com>
2022-05-16 14:51:38 -0700

    Update module_report/check_apis.py
    Co-authored-by: CAM Gerlach <CAM.Gerlach@Gerlach.CAM>

--- module_report/check_apis.py
+++ module_report/check_apis.py
@@ -8,15 +8,8 @@ import qtpy
 
 def check_apis(report_name):
     for api in sorted(qtpy.API_NAMES):
-        # this doesn't actually change the QT_API
-        # I would prefer using this if it actually worked
-        # child_env = dict(os.environ).update({'QT_API': api})
-
-        # still required to actually change the selected API
-        os.environ['QT_API'] = api
-
         subprocess.run(
             [sys.executable, Path(__file__).parent / 'check_api.py', report_name],
             check=False, # check_api.py apparently returns a non-zero exit status despite not failing
-            # env=child_env
+            env={**os.environ, 'QT_API': api},
         )
